2dc872bf6b653e5b6fb5c1df0085583452d213b9
Dan Tenenbaum <dtenenba@fhcrc.org>
2016-02-22 15:09:11 -0800

    fix typos, use correct user and no-lock-tables option in mysqldump

--- start.sh
+++ start.sh
@@ -9,17 +9,15 @@ fi
 cd /tmp
 
 echo get database from master
-mysqldump --password=$MYSQL_REMOTE_PASSWORD -u hubuser -h experimenthub.bioconductor.org experimenthub | gzip > /tmp/dump.sql.gz
+mysqldump --lock-tables=false --password=$MYSQL_REMOTE_PASSWORD -u hubuser_readonly -h experimenthub.bioconductor.org experimenthub | gzip > /tmp/dump.sql.gz
 
 echo create local database and user
 cat /tmp/start.sql | sed "s/MYSQL_REMOTE_PASSWORD/$MYSQL_REMOTE_PASSWORD/" |  mysql -u root --password=$MYSQL_ROOT_PASSWORD -h db
 
 echo populate local database
-zcat /tmp/dump.sql.gz | mysql -u root --password=$MYSQL_ROOT_PASSWORD -h db
-experimenthub
+zcat /tmp/dump.sql.gz | mysql -u root --password=$MYSQL_ROOT_PASSWORD -h db experimenthub
 
-curl http://experimenthub.bioconductor.org:3000/metadata/experimenthub.sqlite3 >
-/HubServer/experimenthub.sqlite3
+curl http://experimenthub.bioconductor.org:3000/metadata/experimenthub.sqlite3 > /HubServer/experimenthub.sqlite3
 
 cd /HubServer
 
@@ -30,4 +28,3 @@ echo start app
 shotgun -o 0.0.0.0 -p 3000 app.rb
 
 sleep infinity
-
